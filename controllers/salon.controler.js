6de4aaacbbf54aae7e5bb1848793579cbdf8f3aa
MarcelBragance <bragance.m@hotmail.fr>
2021-05-10 12:22:56 +0200

    admin back office

--- controllers/salon.controler.js
+++ controllers/salon.controler.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
-
+const salonModel = require("../models/salon.modele");
 const fs = require("fs");
 
 
 
 exports.salon_affichage= (requete,reponse)=>{
-
+    
         reponse.render("chaat_rooms/chat.html.twig")
 }
 
 
 
 
-exports.liste_salon_affichage= (requete,reponse)=>{
+exports.liste_salon_affichage = (requete,reponse)=>{
+    salonModel.find()
+    .exec()
+    .then( salon=>{
+        reponse.render("chaat_rooms/liste_chat.html.twig",{
+            liste : salon
+        });
 
-    reponse.render("chaat_rooms/liste_chat.html.twig")
+    })
+    .catch( error =>{
+        console.log(error)
+    });     
 }
